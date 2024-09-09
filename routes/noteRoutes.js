d29389857aadca6ef8c4bed60081b472b3497c3e
Gaudeamus013 <reynaldo.javier@hotmail.com>
2024-09-09 01:24:20 -0300

    noteRoutes.js modified

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router();
-const { createNote, getNotes, getNoteById, updateNote, deleteNote } = require('../controllers/noteController');
+const {createNote, getAllNotes, getNoteById, updateNoteById, deleteNoteById} = require('../controllers/noteController');
+const noteRouter = express.Router();
+
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/create', protect, createNote);
-router.get('/readall', protect, getNotes);
-router.get('/readone/:id', protect, getNoteById);
-router.put('/update/:id', protect, updateNote);
-router.delete('/delete/:id', protect, deleteNote);
+noteRouter.post('/create', protect, createNote);
+noteRouter.get('/readall', protect, getAllNotes);
+noteRouter.get('/readone/:id', protect, getNoteById);
+noteRouter.put('/update/:id', protect, updateNoteById);
+noteRouter.delete('/delete/:id', protect, deleteNoteById);
 
-module.exports = router;
+module.exports = noteRouter;
