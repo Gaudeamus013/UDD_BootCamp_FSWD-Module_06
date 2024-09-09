52d33779bc330e1c05b0607b27e4aa450f203d88
Gaudeamus013 <reynaldo.javier@hotmail.com>
2024-09-09 01:25:30 -0300

    userRoutes.js modified

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser, verifyToken, updateUser } = require('../controllers/userController');
+const { createUser, login, verifyToken, updateUser, getAllUsers } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/verifytoken', protect, verifyToken);
-router.put('/update', protect, updateUser);
+const userRoutes = express.Router();
 
-module.exports = router;
+userRoutes.post('/register', createUser);
+userRoutes.post('/login', login);
+userRoutes.get('/verifytoken', protect, verifyToken);
+userRoutes.get('/users', protect, getAllUsers);
+userRoutes.put('/update', protect, updateUser);
+
+module.exports = userRoutes;
