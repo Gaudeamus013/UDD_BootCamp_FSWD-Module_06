97fcc481ac7b1d24a2dce70fae2c7ff66a1062e9
Gaudeamus013 <reynaldo.javier@hotmail.com>
2024-09-09 01:09:25 -0300

    database.js modified

--- config/database.js
+++ config/database.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI );
+    console.log('Conectado a la BD de MongoDB');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
